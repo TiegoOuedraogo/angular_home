67f67e46f3afc0d3b3101f41289622a70bba3b4b
tiegoouedraogo <tiego.ouedraogo@peopleshores.com>
2025-01-02 12:32:14 -0500

    data binding, angular interpolation and angular syntax implemented

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {Housinglocation} from "../housinglocation";
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section class="listing">
+      <!--[src] is the property binding.
+           which tells angular the the value in
+           quote is an actual propertyt from the component class.
+
+           the double curly braces is the angular interpolation of
+
+           -->
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{housingLocation.name}}</h2>
+      <p class="listing-location">{{housingLocation.city}},{{housingLocation.state}}</p>
+    </section>
   `,
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
+//   input Decorator
+  @Input() housingLocation!: Housinglocation;
 
 }
